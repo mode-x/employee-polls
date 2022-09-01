8d4089b1e599d0428718c34fbb236a85a0ce28bd
mode-x <abiaemma@gmail.com>
2022-09-01 11:56:56 +0100

    refactor: Add scaffold for querying

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,14 +1,17 @@
-import { _getUsers, _getQuestions } from "../utils/_DATA";
+import { _getUser, _getUsers, _getQuestions } from "../utils/_DATA";
 import { receivePolls } from "./polls";
 import { receiveUsers } from "./users";
 import { setAuthedUser } from "./authedUser";
 
-const AUTHED_ID = "tylermcginnis";
+const AUTHED_ID = null;
 
 export function handleInitialData() {
   return async (dispatch) => {
     const polls = await _getQuestions();
+    const user = await _getUser("tylermcginnis", "abc321");
     const users = await _getUsers();
+
+    console.log(user);
 
     dispatch(receivePolls(polls));
     dispatch(receiveUsers(users));
--- src/component/Login.js
+++ src/component/Login.js
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import { _getUser } from "../utils/_DATA";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUserIdChange = (event) => {
+    setUserId(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    console.log(userId, password);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-form">
+        <form>
+          <label>Username:</label>
+          <br />
+          <input
+            type="text"
+            name="username"
+            data-testid="username-input"
+            value={userId}
+            onChange={handleUserIdChange}
+          />
+          <br />
+          <label>Password:</label>
+          <br />
+          <input
+            type="text"
+            name="password"
+            data-testid="password-input"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <br />
+          <input type="submit" value="Submit" onClick={login} />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;

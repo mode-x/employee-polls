9d0b8db301e71cd76c72a0d66e2a9b50e15ba3ca
mode-x <abiaemma@gmail.com>
2022-09-02 06:28:37 +0100

    refactor: Add logout function to NavBar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,8 +1,15 @@
 import "../assets/css/W3.css";
 import { Link } from "react-router-dom";
+import { setAuthedUser } from "../actions/authedUser";
 import { connect } from "react-redux";
 
 const NavBar = (props) => {
+  const logout = (event) => {
+    event.preventDefault();
+
+    props.dispatch(setAuthedUser(null));
+  };
+
   return (
     <div className="w3-bar w3-border w3-light-grey">
       <Link to="/" className="w3-bar-item w3-button">
@@ -24,7 +31,11 @@ const NavBar = (props) => {
             alt="Alps"
           ></img>
         </span>
-        <Link to="/add" className="w3-bar-item w3-button w3-green">
+        <Link
+          to="/add"
+          className="w3-bar-item w3-button w3-green"
+          onClick={logout}
+        >
           Logout
         </Link>
       </div>
